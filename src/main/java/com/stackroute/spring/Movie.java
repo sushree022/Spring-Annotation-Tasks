12c572da2f0deacb4d1f358b4095e2848831b438
sushree mishra <sushree022@gmail.com>
2019-07-29 16:01:20 +0530

    Task4: Movie Project using annotation which implemented ApplicationContextAware, BeanFactoryAware and BeanNameAware in the movie class is added

--- src/main/java/com/stackroute/spring/Movie.java
+++ src/main/java/com/stackroute/spring/Movie.java
@@ -1,10 +1,16 @@
 package com.stackroute.spring;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 import java.util.List;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     @Autowired
     //    instance of Actor class created
     private List<Actor> actor;
@@ -26,6 +32,23 @@ public class Movie {
         this.actor = actor;
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean factory is set: "+beanFactory);
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean Name is set: " + s);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context is set: "+applicationContext);
+
+    }
     //    to String method over ridden
 
     @Override
